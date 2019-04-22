0ca982aa3885e64ae97fb18e6cf359893a0ba7e6
vagrant <vagrant@vagrant.vm>
2019-04-22 06:55:45 +0000

    adding a function

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,22 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+files = {"um-deliveries-20140519.txt" : 1, 
+"um-deliveries-20140520.txt" : 2,
+ "um-deliveries-20140521.txt" : 3}
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+day = 1
+file = "um-deliveries-20140519.txt"
+def melon_delivery_report(day, file):
+        print(day)
+        the_file = open(file)
+        for line in the_file:
+                line = line.rstrip()
+        words = line.split('|')
 
-    print("Delivered {} {}s for total of ${}".format(
+        melon = words[0]
+        count = words[0]
+        amount = words[0]
+
+        print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
-the_file.close()
+        the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+print(melon_delivery_report(day, file))
