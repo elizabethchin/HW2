f639ce4ae55fdc85de56fe791d4e9ef8b5690984
vagrant <vagrant@vagrant.vm>
2019-04-22 07:09:45 +0000

    fixing index

--- produce_summary.py
+++ produce_summary.py
@@ -1,22 +1,22 @@
-files = {"um-deliveries-20140519.txt" : 1, 
-"um-deliveries-20140520.txt" : 2,
- "um-deliveries-20140521.txt" : 3}
+files = {"um-deliveries-20140519.txt" : "Day 1", 
+"um-deliveries-20140520.txt" : "Day 2",
+ "um-deliveries-20140521.txt" : "Day 3"}
 
-day = 1
+day = "1"
 file = "um-deliveries-20140519.txt"
-def melon_delivery_report(day, file):
-        print(day)
+def melon_delivery_report(files):
+        print("Day " + day)
         the_file = open(file)
         for line in the_file:
                 line = line.rstrip()
-        words = line.split('|')
+                words = line.split('|')
 
-        melon = words[0]
-        count = words[0]
-        amount = words[0]
+                melon = words[0]
+                count = words[1]
+                amount = words[2]
 
-        print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+                print("Delivered {} {}s for total of ${}".format(
+                count, melon, amount))
         the_file.close()
 
 print(melon_delivery_report(day, file))
